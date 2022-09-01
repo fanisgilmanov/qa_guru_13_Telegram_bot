e36b3daebf08d5c95d9951d656ae979996b30568
fanisgilmanov <fanisgilmanov1994@gmail.com>
2022-09-01 20:53:07 +0500

    add allure notification 4.2.1

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -13,14 +13,7 @@ public class BaseTest {
     static void beforeUrl() {
         SelenideLogger.addListener("allure", new AllureSelenide());
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
 
-        Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterEach
